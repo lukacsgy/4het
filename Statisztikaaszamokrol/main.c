9e58702d5e91dcabb5b993abcc0ca485f6e844cd
György Lukács <georgelucasletters@gmail.com>
2018-01-27 12:56:59 +0100

    Írjunk programot, amelyik a szabványos bemenetről olvas számokat, ameddig csak tud. Számolja meg, hogy a beírt számok közül, amelyek 1 és 10 között vannak, melyik hányszor szerepelt! Írja ki ezt a szabványos kimenetre az oldalt látható formában!

---
+++ Statisztikaaszamokrol/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int szam, i, darab[10] = {0};
+
+    printf("Add meg a szamokat 1-10 között: ");
+
+    while(scanf("%d", &szam) == 1){
+        if (szam > 0 && szam <= 10){
+            darab[szam - 1] += 1;
+        }
+    }
+
+    for (i = 1; i < 10; i++){
+        printf("%2d: %d db\n", i, darab[i - 1]);
+    }
+    return 0;
+}
