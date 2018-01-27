62176fe6a92c9cedff4812d98c33b6ef7d5dad31
György Lukács <georgelucasletters@gmail.com>
2018-01-27 13:02:04 +0100

    Done

--- Statisztikaaszamokrol/main.c
+++ Statisztikaaszamokrol/main.c
@@ -4,15 +4,15 @@
 int main(void){
     int szam, i, darab[10] = {0};
 
-    printf("Add meg a szamokat 1-10 között: ");
+    printf("Add meg a szamokat 1-10 között (kilépés bármilyen nem szam): ");
 
     while(scanf("%d", &szam) == 1){
-        if (szam > 0 && szam <= 10){
-            darab[szam - 1] += 1;
+        if (szam > 0 && szam < 11){
+            darab[szam - 1]++;
         }
     }
 
-    for (i = 1; i < 10; i++){
+    for (i = 1; i <= 10; i++){
         printf("%2d: %d db\n", i, darab[i - 1]);
     }
     return 0;
